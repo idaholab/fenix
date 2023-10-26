5702e369c578e2c5e4f7df32c93fc9edd5a36c65
Logan Harbour <loganhharbour@gmail.com>
2023-10-26 12:45:29 -0500

    Initial files

---
+++ src/base/FenixApp.C
@@ -0,0 +1,50 @@
+#include "FenixApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+FenixApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  return params;
+}
+
+FenixApp::FenixApp(InputParameters parameters) : MooseApp(parameters)
+{
+  FenixApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+FenixApp::~FenixApp() {}
+
+void 
+FenixApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ModulesApp::registerAllObjects<FenixApp>(f, af, s);
+  Registry::registerObjectsTo(f, {"FenixApp"});
+  Registry::registerActionsTo(af, {"FenixApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+FenixApp::registerApps()
+{
+  registerApp(FenixApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+FenixApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  FenixApp::registerAll(f, af, s);
+}
+extern "C" void
+FenixApp__registerApps()
+{
+  FenixApp::registerApps();
+}
---
+++ test/src/base/FenixTestApp.C
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "FenixTestApp.h"
+#include "FenixApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+
+InputParameters
+FenixTestApp::validParams()
+{
+  InputParameters params = FenixApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  return params;
+}
+
+FenixTestApp::FenixTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  FenixTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+FenixTestApp::~FenixTestApp() {}
+
+void
+FenixTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  FenixApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"FenixTestApp"});
+    Registry::registerActionsTo(af, {"FenixTestApp"});
+  }
+}
+
+void
+FenixTestApp::registerApps()
+{
+  registerApp(FenixApp);
+  registerApp(FenixTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+FenixTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  FenixTestApp::registerAll(f, af, s);
+}
+extern "C" void
+FenixTestApp__registerApps()
+{
+  FenixTestApp::registerApps();
+}
