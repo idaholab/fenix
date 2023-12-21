6f2948371368d29e44b8519c7140875b9d0c7d6f
Logan Harbour <loganhharbour@gmail.com>
2023-12-21 12:58:25 -0700

    Change test to also test caching

--- test/src/userobjects/TestAuxAccumulator.C
+++ test/src/userobjects/TestAuxAccumulator.C
@@ -33,27 +33,31 @@ TestAuxAccumulator::execute()
 {
   FENIX::AuxAccumulator accumulator(_fe_problem, getParam<AuxVariableName>("variable"));
 
-  for (const auto & elem : _fe_problem.mesh().getMesh().active_local_element_ptr_range())
-  {
-    const auto add = [&elem, &accumulator](const Point & point)
-    { return accumulator.add(*elem, point, point(0) + 2 * point(1) + 3 * point(2)); };
+  const auto add = [&accumulator](const Elem & elem, const Point & point)
+  { return accumulator.add(elem, point, point(0) + 2 * point(1) + 3 * point(2)); };
 
+  for (const auto & elem : *_fe_problem.mesh().getActiveLocalElementRange())
+  {
     const auto centroid = elem->vertex_average();
 
     // At the centroid
-    add(centroid);
+    add(*elem, centroid);
 
+    // Every node but the first (see next elem loop for why)
     for (const auto n : elem->node_index_range())
-    {
-      const auto & node = elem->point(n);
+      if (n != 0)
+        add(*elem, elem->point(n));
 
-      // At each node
-      add(node);
-
-      // Towards the centroid from each node
-      add(0.5 * (centroid + node));
-    }
+    // Towards the centroid from each node
+    for (const auto n : elem->node_index_range())
+      add(*elem, 0.5 * (centroid + elem->point(n)));
   }
+
+  // Node 0 for every element
+  // We do a second loop here so we test the caching capability
+  // (calling for the same element out of order, only one per element)
+  for (const auto & elem : *_fe_problem.mesh().getActiveLocalElementRange())
+    add(*elem, elem->point(0));
 
   if (!getParam<bool>("skip_finalize"))
     accumulator.finalize();
